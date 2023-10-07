71036f583250d3483f8e283d12156275e1b03703
Francis Bello <bellofrancis87@gmail.com>
2023-10-07 20:37:25 +0100

    changed the color theme

--- app/component/Nav/Navigation.jsx
+++ app/component/Nav/Navigation.jsx
@@ -11,21 +11,28 @@ export default function Navigation() {
   useScrollAnimaton();
 
   return (
-    <nav className="fixed w-full top-0 left-0 z-50">
+    <nav className="fixed w-full top-0 left-0 z-50 bg-gray-200 text-gray-800">
       <div className="marker fixed top-[1.75rem] left-[4rem] w-[1rem] h-[1rem]" />
-      <div className="nav-track font-lobster relative h-[6rem]" data-draggable>
+      <div className="nav-track  relative h-[6rem]" data-draggable>
         <ul className="nav-list p-0 m-0 flex justify-between">
-          {NAV_DATA.map((item, index) => (
-            <li key={index}>
-              <Link
-                href={`#${SECTIONS_DATA[index].title}`}
-                className="nav-link"
-                data-link
-              >
-                {item}
-              </Link>
-            </li>
-          ))}
+          {NAV_DATA.map((item, index) => {
+            const isEvenIndex = index % 2 === 0;
+            const linkColor = isEvenIndex
+              ? "text-white hover:text-yellow-400"
+              : "text-gray-800 hover:text-blue-900";
+
+            return (
+              <li key={index}>
+                <Link
+                  href={`#${SECTIONS_DATA[index].title}`}
+                  className="nav-link "
+                  data-link
+                >
+                  {item}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
--- app/page.js
+++ app/page.js
@@ -6,29 +6,36 @@ import useScrollContentAnimation from "./hooks/useScrollContentAnimation";
 
 export default function Home() {
   useScrollContentAnimation();
+  const bgColors = ["bg-blue-900", "bg-gray-100"];
 
   return (
     <div className="">
       <Navigation />
-      <main className="mt-28">
+      <main className="mt-24">
         {SECTIONS_DATA.map((section, index) => {
-          const { src, title, year, backgroundColor } = section;
+          const { src, title, year } = section;
+          const isEvenIndex = index % 2 === 0;
+          const yearColor = isEvenIndex ? "text-gray-800" : "text-white";
+          const bgColorClass = isEvenIndex
+            ? "bg-gray-200 text-yellow-600"
+            : "bg-blue-900 text-yellow-400";
           return (
             <section
-              className=" flex items-center justify-center"
+              className={`flex items-center justify-center ${bgColorClass}`}
               id={section.title}
               key={index}
-              style={{ backgroundColor: backgroundColor }}
             >
               <div className=" container grid md:grid-cols-2 grid-cols-1 w-full max-w-[80rem]">
                 <h2 className="heading flex flex-col">
-                  <span className=" text-base text-white">{year}</span>
-                  <span className=" md:text-7xl text-2xl font-bold  whitespace-pre-wrap uppercase font-monte">
+                  <span className={`" text-base font-lobster " ${yearColor}`}>
+                    {year}
+                  </span>
+                  <span className=" md:text-7xl text-2xl font-bold  whitespace-pre-line uppercase font-monte">
                     {title}
                   </span>
                 </h2>
                 <div className="section-img">
-                  <img src={src} alt={section.title} />
+                  <img src={src} alt={section.title} className=" w-full" />
                 </div>
               </div>
             </section>
